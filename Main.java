6f56461ed30c80cc333c4d99788392a5e3defcbb
luongthaithuong <luongthuong6688@gmail.com>
2024-01-18 19:18:30 +0700

    update connection

--- Main.java
+++ Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("createtablecoffee.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
